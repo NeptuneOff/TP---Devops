b2c10bfefbba0b5c9b5432723167cd93a9d1bf49
NeptuneOff <kdl33110@gmail.com>
2026-06-02 10:39:23 +0200

    TP02 et correction readme et tp1

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 cd TP_WIK_DPS_01
+node server.js
 
+
+cd TP_WIK_DPS_02
 docker build -t ping-api .
-
 docker run --rm -p 3000:3000 ping-api
---
+++ TP_WIK_DPS_02/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const http = require("http");
+
+const port = process.env.PING_LISTEN_PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/ping") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(JSON.stringify(req.headers, null, 2));
+    return;
+  }
+
+  res.writeHead(404, {
+    "Content-Type": "application/json",
+  });
+
+  res.end(JSON.stringify({ error: "Route not found" }));
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
